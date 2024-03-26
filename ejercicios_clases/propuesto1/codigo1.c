9991660f99d96a37ecb367729b5fc7b2d073defe
Loreto <loreto.nancucheo@gmail.com>
2024-03-26 17:22:36 -0300

    Add code for propuesto1 exercise

---
+++ ejercicios_clases/propuesto1/codigo1.c
@@ -0,0 +1,27 @@
+/*
+    Ejemplo Vistó en Clase
+    Control 1, 2023-02 
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int counter = 0; //variable global
+int main(){ //codigo1 
+    pid_t r;
+    while(counter < 2){
+        r= fork();
+        if(r > 0 ){
+            execlp("./codigo2", "", NULL);
+        }
+        counter = counter + 1;
+
+        printf("%d\n", counter);
+    }
+
+    return 0;
+}
---
+++ ejercicios_clases/propuesto1/codigo2.c
@@ -0,0 +1,26 @@
+/*
+    Ejemplo Vistó en Clase
+    Control 1, 2023-02 
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int counter = 0; //variable global
+int main(){ //codigo2
+    pid_t t;
+    t = fork();
+    if( t > 0 ){
+        counter = counter * 2;
+    }
+    else {
+        counter = counter -1;
+    }
+    printf("%d\n", counter);
+
+    return 0;
+}
